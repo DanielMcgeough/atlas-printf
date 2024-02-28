26200d14e2115954c792165ef257cdbfa90cdc72
Your Name <you@example.com>
2024-02-28 16:05:43 +0000

    added string.h

--- main.h
+++ main.h
@@ -5,14 +5,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <string.h>
 
-/**
- * _printf - prints whatever is fed into argument
- * @format: character strings
- * @...: whatever else we need to pass in
- * Return: returns the number of chars printed
- */
+
 
 int _printf(const char *format, ...);
-
 
