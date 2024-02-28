5cd58434f36d3e3d66f0e6a80f862a7daa583f90
Your Name <you@example.com>
2024-02-28 19:11:56 +0000

    fixed comment block and added func name

--- _printf.c
+++ _printf.c
@@ -6,33 +6,35 @@
  * @...: whatever else we need to pass in
  * Return: returns the number of chars printed
  */
-/*
+/**
 int _printf(const char *format, ...)
 {
 	int size;
 	va_list args;
 	char str;
 	int *handler;
-/* if format argument is NULL, return -1 */
-/*	if (format == NULL)
+ if format argument is NULL, return -1 
+	if (format == NULL)
 		return (-1);
-/* calculate size of string, if <= 0, return 0 */
-/*	size = strlen(format);
+/* calculate size of string, if <= 0, return 0 
+	size = strlen(format);
 	if (size <= 0)
 		return (0);
-/* initialize list named args, setting handler = to size */
-/*	va_start(args, format);
+/* initialize list named args, setting handler = to size 
+	va_start(args, format);
 	size = *handler(format, args);
-/* clean up the list */
-/*	va_end(args);
-/* check for end of argument */
-/*	str = va_arg(args, char)
+/* clean up the list 
+	va_end(args);
+/* check for end of argument 
+	str = va_arg(args, char)
 	while ((str != '\0')
 		str++;
 
 	return (size);
 }
 */
+int _printf(const char *format, ...)
+{
 va_list args;
 va_start(args, format);
 /* looping through string to find format specifiers */
@@ -56,3 +58,5 @@ va_start(args, format);
 	}
 			va_end(args);
 		}
+	}
+}
