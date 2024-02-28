ffbdd1e6d10647f6128108b2e64e871a2198b17a
Your Name <you@example.com>
2024-02-28 16:35:06 +0000

    Added a loop to printf.c and created the man page file

--- _printf.c
+++ _printf.c
@@ -11,18 +11,22 @@ int _printf(const char *format, ...)
 {
 	int size;
 	va_list args;
-
+	char = str;
+/* if format argument is NULL, return -1 */
 	if (format == NULL)
 		return (-1);
-
+/* calculate size of string, if <= 0, return 0 */
 	size = _strlen(format);
 	if (size <= 0)
 		return (0);
-
+/* initialize list named args, setting handler = to size */
 	va_start(args, format);
 	size = handler(format, args);
-
+/* clean up the list */
 	va_end(args);
+/* check for end of argument */
+	while ((str = va_arg(args, char)) != '\0')
+		str++;
 
 	return (size)
 		}
