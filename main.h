1e34caf239abc51b7671cf2425243a2608cff85d
Your Name <you@example.com>
2024-02-28 16:08:11 +0000

    added dox

--- main.h
+++ main.h
@@ -1,13 +1,17 @@
 #ifndef _HEADER_
 #define _HEADER_
 
+/** we decided to include every lib we could think of to make sure
+ * we didn't have seg faults or missing libraries and we included
+ * them here in our header file to cut down on bloating in other
+ * files. This way, they only need the main.h file. 
+ */
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
-
-
 
 int _printf(const char *format, ...);
 
