7fb5fc843eda5b3e2ba7f8a99a66a27994b295b4
Your Name <you@example.com>
2024-02-28 16:00:32 +0000

    file creation

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - prints whatever is fed into argument
+ * @format: character strings
+ * @...: whatever else we need to pass in
+ * Return: returns the number of chars printed
+ */
+
+int _printf(const char *format, ...)
+{
+
---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _printf - prints whatever is fed into argument
+ * @format: character strings
+ * @...: whatever else we need to pass in
+ * Return: returns the number of chars printed
+ */
+
+int _printf(const char *format, ...);
+
+
