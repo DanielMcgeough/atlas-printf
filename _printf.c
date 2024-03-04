3371d73f5d23df7be65ec35461b1a5cea596562b
Your Name <you@example.com>
2024-03-04 15:20:19 +0000

    wrong quotes

--- _printf.c
+++ _printf.c
@@ -51,7 +51,7 @@ int _printf(const char *format, ...)
 				write(1, format, 1);
 				chara_print++;
 			}
-			else if (*format == "%" && *format + 1 == '\0')
+			else if (*format == '%' && *format + 1 == '\0')
 			{
 				return (-1);
 			}
